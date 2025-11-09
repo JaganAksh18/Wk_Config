3bbe035eae6b35bbd49fda0bae7551e17c210a1b
jaganAksh <djagadeesan42@gmail.com>
2025-11-10 02:03:21 +0530

    add

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Set base to the repository name so assets are served from
-// https://<username>.github.io/Wk_Config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/Wk_Config/',
-})
